75c0c2c5449e981a6b8e4ead345ab3e350429db2
Naveed <mastermindgeh@gmail.com>
2024-02-03 22:42:51 +0500

    add mongodb conntion

---
+++ src/config/config.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const envVarsSchema = Joi.object().keys({
+  NODE_ENV: Joi.string().valid("production", "development", "test"),
+});
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+export const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  mongoose: {
+    url: envVars.MONGODB_URL,
+  },
+};
